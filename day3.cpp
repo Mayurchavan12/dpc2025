a93d57cd2351333fea3c4a3c5cd742bf8b7fd521
Mayurchavan12 <120295976+Mayurchavan12@users.noreply.github.com>
2025-08-17 13:33:51 +0530

    Create day3.cpp
    Day 3 challenge

---
+++ day3.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(vector<int>& arr) {
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    slow = arr[0];
+    while (slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    return slow;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = findDuplicate(arr);
+    cout << "Duplicate number: " << result << endl;
+
+    return 0;
+}
