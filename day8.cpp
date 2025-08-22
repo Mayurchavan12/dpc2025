eba79a8c80ffa3e336eede929f8301381d03845f
Mayurchavan12 <120295976+Mayurchavan12@users.noreply.github.com>
2025-08-22 17:22:49 +0530

    Create day8.cpp
    Day 8 challenge

---
+++ day8.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWordsString(string s){
+
+    int n = s.length();
+    string ans = "";
+
+    reverse(s.begin(), s.end());
+
+    for(int i=0; i<n; i++){
+        string word = "";
+        while(i<n && s[i] != ' '){
+            word += s[i];
+            i++;
+        }
+
+        reverse(word.begin(), word.end());
+        if(word.length() > 0){
+            ans += " " + word;
+        }
+    }
+
+    return ans.substr(1);
+}
+
+int main(){
+    string s;
+    cout << "Enter the string: ";
+    getline(cin, s);
+    cout << reverseWordsString(s) << endl;
+    return 0;
+}
